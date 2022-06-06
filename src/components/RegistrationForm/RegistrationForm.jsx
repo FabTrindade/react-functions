a97745dc3b0a959b5eb1710b3b205cca527dcc4e
FabTrindade <fabrtrindade@gmail.com>
2022-06-05 21:43:19 -0300

    Using Material-UI Stepper.

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,3 +1,4 @@
+import { StepLabel, Stepper, Typography, Step } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import UserData from '../UserData';
 import AddressData from './AddressData';
@@ -6,19 +7,22 @@ import PersonalData from './PersonalData';
 function RegistrationForm({ atSend, validateIdCard }) {
 
     const [step, setStep] = useState(0);
-    const [data, setData]= useState({});
+    const [data, setData] = useState({});
 
-    useEffect(()=>{
-        atSend({data});
+    useEffect(() => {
+        if (step === (forms.length - 1)) {
+            atSend({ data });
+        }
     })
     const forms = [
         <UserData atSend={collectData} />,
         <PersonalData atSend={collectData} validateIdCard={validateIdCard} />,
-        <AddressData atSend={collectData} />
+        <AddressData atSend={collectData} />,
+        <Typography variant='h5'>Thanks for signing up!</Typography>
     ]
 
     function collectData(newData) {
-        setData({...data, ...newData});
+        setData({ ...data, ...newData });
         next();
     }
 
@@ -28,6 +32,13 @@ function RegistrationForm({ atSend, validateIdCard }) {
 
     return (
         <>
+            <Stepper activeStep={step}>
+                <Step><StepLabel>Login</StepLabel></Step>
+                <Step><StepLabel>Personal data</StepLabel></Step>
+                <Step><StepLabel>Address</StepLabel></Step>
+                <Step><StepLabel>Finished</StepLabel></Step>
+
+            </Stepper>
             {forms[step]}
         </>
     )
